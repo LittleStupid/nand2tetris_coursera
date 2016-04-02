375588682691ef3668401849c081b1d598f14124
LittleStupid <lttlstpd@live.com>
2016-04-02 18:50:34 +0800

    project4 done.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,98 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+//IT COST ME A WHOLE DAY TO WORK IT OUT......WTF
+
+//ROW_NUM=256
+//COL_NUM=512
+//MEM_NUM=256*512/16=8192
+//INIT:SET R0 as 8192
+
+	//R0 = 8192
+	@8192
+	D=A
+	@R0
+	M=D
+	
+	//R1 is -1
+	@R1
+	M=-1
+	
+(LOOP)
+	//R2 is i,set as 0 erery time
+	@R2
+	M=0
+	
+	@KBD
+	D=M
+	
+	@BLACK
+	D;JGT
+	
+(WHITE)
+	//get i
+	@R2
+	D=M
+	
+	//fill screen[i] as 0
+	@SCREEN
+	A=A+D
+	M=0
+	
+	//i++
+	@R2
+	M=M+1
+
+	//fill enough memory blocks? if so,back to loop
+	@R0
+	D=M
+	
+	//compare with iterator
+	@R2
+	D=D-M
+	
+	//end inner loop if needed
+	@LOOP
+	D;JLE
+	
+	//continue fill black
+	@WHITE
+	0;JMP
+//WHITE END
+	
+
+(BLACK)
+	//get i
+	@R2
+	D=M
+	
+	//fill screen[i] as -1
+	@SCREEN
+	A=A+D
+	M=-1
+	
+	//i++
+	@R2
+	M=M+1
+	
+	//fill enough memory blocks? if so,back to loop
+	@R0
+	D=M
+	
+	//compare with iterator
+	@R2
+	D=D-M
+	
+	//end inner loop if needed
+	@LOOP
+	D;JLE
+	
+	//continue fill black
+	@BLACK
+	0;JMP
+//BLACK END
+
+	@LOOP
+	0;JMP
+//END LOOP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R0
+D=M
+
+@R4	//TEMP: RESTORE R0
+M=D
+
+@R0
+M=0
+	
+(LOOP)
+	@R1
+	D=M
+	
+	@DONE
+	D;JLE
+	
+	@R4
+	D=M
+	
+	@R0
+	M=D+M
+	
+	@R1
+	M=M-1
+	
+	@LOOP
+	0;JMP
+	
+(DONE)
+	@R0
+	D=M
+	
+	@R2
+	M=D
+	
+(END)
+	@END
+	0;JMP
